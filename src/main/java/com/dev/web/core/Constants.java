122700def1c58bbdd5a61213e2b2e25906119063
Mykyta_Ovsiannikov <Mykyta_Ovsiannikov@epam.com>
2017-11-09 13:29:36 +0200

    small update

--- src/main/java/com/dev/web/core/Constants.java
+++ src/main/java/com/dev/web/core/Constants.java
@@ -13,8 +13,10 @@ public class Constants {
     public static final String KYIV_ID = "2200001";
     public static final String RIVNE = "Ровно";
     public static final String RIVNE_ID = "2218400";
-    public static final String DATE_DEP = "27.10.2017";
-    public static final String TRAIN = "097К";
+    public static final String DATE_DEP = "10.11.2017";
+    public static final String TRAIN = "705Л";
+    public static final String TRAIN_705L = "705Л";
     public static final String CLASS = "П";
     public static final String CLASS_2 = "K";
+    public static final String CLASS_IC_C2 = "С2";
 }
